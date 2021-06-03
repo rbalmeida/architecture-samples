4fc32dcdbe1a7957e9601897d2f4c62919b9b401
ralmeida <rafael.b.almeida@gmail.com>
2021-06-03 20:47:10 -0300

    Kubernetes Experiments: Python interprocess communication with RabbitMQ.
    Modification of sender script to continuously and randomly send messages.

--- kubernetes/python/py-rabbit-mq/send.py
+++ kubernetes/python/py-rabbit-mq/send.py
@@ -1,14 +1,18 @@
 import pika
+import random as rd
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='hello world')
-
-print(" sent hello world ")
-connection.close()
+try:
+    while True:
+        channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body='Hello. Random int: {}'.format(rd.randint(1, 1000)))
+        time.sleep(rd.randint(1, 10))
+finally:
+    connection.close()
 
 # Ref: https://www.rabbitmq.com/tutorials/tutorial-one-python.html
