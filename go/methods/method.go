d0e2731df405aac244a74ee441c562a509becfcf
rafael.b.almeida@gmail.com <rafael.b.almeida@gmail.com>
2022-10-17 19:25:03 -0300

    Go methods

--- go/methods/method.go
+++ go/methods/method.go
@@ -9,11 +9,19 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(s float64) {
+	v.X *= s
+	v.Y *= s
 }
 
 func main() {
 	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	v.Scale(2.0)
+	fmt.Println(v)
 	fmt.Println(v.Abs())
 }
